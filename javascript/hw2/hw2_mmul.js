57d614aca2e74b6b4d26cf9ed4f3d5118d7338b9
ctfpanda17 <r61291@gmail.com>
2023-03-30 21:55:22 +0800

    hw2

--- javascript/hw2/hw2_mmul.js
+++ javascript/hw2/hw2_mmul.js
@@ -1,18 +1,16 @@
 function mmul(a, b) {
-    let r = [[], []]
-    let sum=0
+    let r=[[0,0],[0,0]]
     for (let i = 0; i < a.length; i++) {
-        for (let j = 0; j < a[0].length; j++) {
-            for (let k=0 ; k<b.length; k++) {
-                sum += a[i][k] * b[k][j];
-            }
-            r[i][j] = sum
+      for (let j = 0; j < b[0].length; j++) {
+        for (let k = 0; k < b.length; k++) {
+          r[i][j] += a[i][k] * b[k][j];
         }
+      }
     }
-    return r
-}
-
-let a = [[1, 2], [3, 4]]
-let b = [[1, 1], [1, 1]]
-let c = mmul(a, b)
+    
+    return r;
+  }
+let a =[[1,2],[3,4]]
+let b =[[1,1],[1,1]]
+let c = mmul(a,b)
 console.log(c)
